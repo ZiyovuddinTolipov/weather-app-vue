d7b1471a84398f11312ec39758d7dc5cc34f5ec6
tolipov <ziyovuddin.developer@gmail.com>
2023-12-28 03:57:20 +0500

    Navigation

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
-  <div>
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+    <SiteNavigation />
     <RouterView />
   </div>
 </template>
-<script>
+<script setup>
 import { RouterView } from 'vue-router';
+import SiteNavigation from './components/SiteNavigation.vue';
 
 </script>
 <style lang="">
---
+++ src/components/SiteNavigation.vue
@@ -0,0 +1,8 @@
+<template>
+    <header>
+        <nav class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6"></nav>
+    </header>
+</template>
+<script setup>
+ 
+</script>
