5cb919ff224ccd20d7940ce80f85286fb8761cf7
tolipov <ziyovuddin.developer@gmail.com>
2023-12-29 00:04:41 +0500

    app header

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -1,8 +1,20 @@
 <template>
-    <header>
-        <nav class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6"></nav>
+    <header class="sticky top-0 bg-weather-primary shadow-lg">
+        <nav class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6">
+            <RouterLink :to="{ name: 'home' }">
+                <div class="flex items-center gap-3">
+                    <i class="fa-solid fa-sun text-2xl"></i>
+                    <p class="text-2xl">The Local Weather</p>
+                </div>
+            </RouterLink>
+
+            <div class="flex gap-3 flex-1 justify-end">
+                <i class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-200 cursor-pointer"></i>
+                <i class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-200 cursor-pointer"></i>
+            </div>
+        </nav>
     </header>
 </template>
 <script setup>
- 
+import { RouterLink } from 'vue-router';
 </script>
