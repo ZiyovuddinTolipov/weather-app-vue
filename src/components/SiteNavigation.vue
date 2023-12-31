d785366fbc3f42ef2d406eb520208b4cc9a39d3f
tolipov <ziyovuddin.developer@gmail.com>
2024-01-01 02:41:50 +0500

    search city

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -9,39 +9,53 @@
             </RouterLink>
 
             <div class="flex gap-3 flex-1 justify-end">
-                <i class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-200 cursor-pointer" @click="toggleModal"></i>
+                <i class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-200 cursor-pointer"
+                    @click="toggleModal"></i>
                 <i class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-200 cursor-pointer"></i>
             </div>
 
-            <BaseModal 
-            :modalActive="modalActive" 
-            @close-modal="toggleModal">
+            <BaseModal :modalActive="modalActive" @close-modal="toggleModal">
                 <div class="text-black">
                     <h1 class="text-2xl mb-1">About:</h1>
                     <p class="mb-4">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque ducimus non temporibus! Voluptatem
-                        voluptate minima recusandae necessitatibus rerum eveniet! Laboriosam expedita sint quibusdam
-                        cupiditate. Corporis debitis similique quo corrupti inventore!
+                        The Local Weather allows you to track the current and
+                        future weather of cities of your choosing.
                     </p>
-                    <h1 class="text-2xl mb-1">About:</h1>
-                    <p class="mb-4">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque ducimus non temporibus! Voluptatem
-                        voluptate minima recusandae necessitatibus rerum eveniet! Laboriosam expedita sint quibusdam
-                        cupiditate. Corporis debitis similique quo corrupti inventore!
+                    <h2 class="text-2xl">How it works:</h2>
+                    <ol class="list-decimal list-inside mb-4">
+                        <li>
+                            Search for your city by entering the name into the
+                            search bar.
+                        </li>
+                        <li>
+                            Select a city within the results, this will take
+                            you to the current weather for your selection.
+                        </li>
+                        <li>
+                            Track the city by clicking on the "+" icon in the
+                            top right. This will save the city to view at a
+                            later time on the home page.
+                        </li>
+                    </ol>
+
+                    <h2 class="text-2xl">Removing a city</h2>
+                    <p>
+                        If you no longer wish to track a city, simply select
+                        the city within the home page. At the bottom of the
+                        page, there will be am option to delete the city.
                     </p>
                 </div>
-
             </BaseModal>
         </nav>
     </header>
 </template>
 <script setup>
-import {ref} from 'vue';
+import { ref } from 'vue';
 import { RouterLink } from 'vue-router';
 import BaseModal from "./BaseModal.vue"
 
 const modalActive = ref(null);
-const toggleModal =() =>{
+const toggleModal = () => {
     modalActive.value = !modalActive.value;
 }
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,11 @@
         <div class="pt-4 mb-8 relative">
             <input type="text" v-model="searchQuery" @input="getSearchResults"
                 class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]">
+            <ul class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]" v-if="mapboxSearchResults">
+                <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id" class="py-2 cursor-pointer">
+                    {{ searchResult.place_name }}
+                </li>
+            </ul>
         </div>
     </main>
 </template>
