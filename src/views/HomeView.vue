454c6c6ecf069343f5ac2b43e2fac1232d3e239a
tolipov <ziyovuddin.developer@gmail.com>
2024-01-01 21:10:51 +0500

    API Errors/No Data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,10 +3,19 @@
         <div class="pt-4 mb-8 relative">
             <input type="text" v-model="searchQuery" @input="getSearchResults"
                 class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]">
-            <ul class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]" v-if="mapboxSearchResults">
-                <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id" class="py-2 cursor-pointer">
-                    {{ searchResult.place_name }}
-                </li>
+            <ul class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
+                v-if="mapboxSearchResults">
+                <p v-if="searchError">
+                    Sorry, something went wrong, please try again.
+                </p>
+                <p v-if="!serverError && mapboxSearchResults.lenght === 0">
+                    No results match your query, try a different term.
+                </p>
+                <template v-else>
+                    <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id" class="py-2 cursor-pointer">
+                        {{ searchResult.place_name }}
+                    </li>
+                </template>
             </ul>
         </div>
     </main>
