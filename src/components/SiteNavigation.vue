6be931b57fc917aef247c64575f408b8f15c5693
tolipov <ziyovuddin.developer@gmail.com>
2024-01-02 22:29:21 +0500

    bug fix

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class="sticky top-0 bg-weather-primary shadow-lg">
+    <header class="sticky top-0 bg-weather-primary shadow-lg z-40">
         <nav class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6">
             <RouterLink :to="{ name: 'home' }">
                 <div class="flex items-center gap-3">
