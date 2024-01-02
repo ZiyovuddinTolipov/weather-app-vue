2a122abbc7da8e4e5ea56e03e9284a4491813af6
tolipov <ziyovuddin.developer@gmail.com>
2024-01-02 20:23:14 +0500

    add city to localstorage

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -9,9 +9,10 @@
             </RouterLink>
 
             <div class="flex gap-3 flex-1 justify-end">
-                <i class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-200 cursor-pointer"
+                <i class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer"
                     @click="toggleModal"></i>
-                <i class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-200 cursor-pointer"></i>
+                <i class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer"
+                    @click="addCity" v-if="route.query"></i>
             </div>
 
             <BaseModal :modalActive="modalActive" @close-modal="toggleModal">
@@ -49,13 +50,47 @@
         </nav>
     </header>
 </template>
+  
 <script setup>
-import { ref } from 'vue';
-import { RouterLink } from 'vue-router';
-import BaseModal from "./BaseModal.vue"
+import { RouterLink, useRoute, useRouter } from "vue-router";
+import { uid } from "uid";
+import { ref } from "vue";
+import BaseModal from "./BaseModal.vue";
+
+const savedCities = ref([]);
+const route = useRoute();
+const router = useRouter();
+const addCity = () => {
+    if (localStorage.getItem("savedCities")) {
+        savedCities.value = JSON.parse(
+            localStorage.getItem("savedCities")
+        );
+    }
+
+    const locationObj = {
+        id: uid(),
+        state: route.params.state,
+        city: route.params.city,
+        coords: {
+            lat: route.query.lat,
+            lng: route.query.lng,
+        },
+    };
+
+    savedCities.value.push(locationObj);
+    localStorage.setItem(
+        "savedCities",
+        JSON.stringify(savedCities.value)
+    );
+
+    let query = Object.assign({}, route.query);
+    delete query.preview;
+    query.id = locationObj.id;
+    router.replace({ query });
+};
 
 const modalActive = ref(null);
 const toggleModal = () => {
     modalActive.value = !modalActive.value;
-}
+};
 </script>
